45223b9807f4deb54286dae12c1b16f69f49e5af
Russell Gordon <rgordon@lcs.on.ca>
2023-01-22 13:07:26 -0500

    Initial Commit

---
+++ SliderStepExample/SliderStepExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliderStepExampleApp.swift
+//  SliderStepExample
+//
+//  Created by Russell Gordon on 2023-01-22.
+//
+
+import SwiftUI
+
+@main
+struct SliderStepExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
