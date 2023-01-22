475e54bb135bd8c5eff7b2a889d9fb56b7d9ba48
Russell Gordon <rgordon@lcs.on.ca>
2023-01-22 13:19:38 -0500

    Example created to show fraction length and slider with a step value.

--- SliderStepExample/ContentView.swift
+++ SliderStepExample/ContentView.swift
@@ -8,12 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    // MARK: Stored properties
+    @State var selectedNumber: Double = 0.0
+    
+    // MARK: Computed properties
+    
+    // Expresses the user interface
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            
+            Slider(value: $selectedNumber,
+                   in: -50...50,
+                   step: 0.1,
+                   label: { Text("Number") },
+                   minimumValueLabel: { Text("-50") },
+                   maximumValueLabel: { Text("50") })
+            
+            Text("\(selectedNumber.formatted(.number.precision(.fractionLength(2))))")
+                .font(.largeTitle)
         }
         .padding()
     }
